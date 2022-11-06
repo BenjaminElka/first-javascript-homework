fd7387c5de443c3b04c07ed4b76af44ffc9b542c
ben <benjaminelkabas@Benjamins-MacBook-Pro.local>
2022-11-06 21:13:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function orderBurger() {
+   let order= prompt('what would you like on your burger')
+   alert('You ordered a burger with ' + order)
+} 
+
+function orderPizza() {
+   let order= prompt('what would you like on your burger')
+   alert('You ordered a pizza with ' + order)
+}
